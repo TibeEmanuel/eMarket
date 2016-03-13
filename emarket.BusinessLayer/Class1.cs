a2f0f257da3e165b233fc75daf8dadd9cb812fda
Tiebebe <tibuka04@yahoo.com>
2016-03-13 15:04:27 +0100

    bl and dl added

---
+++ emarket.BusinessLayer/Class1.cs
@@ -0,0 +1,18 @@
+﻿using eMarket.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emarket.BusinessLayer
+{
+    public class Class1
+    {
+        DBContext context = new DBContext();
+        public void getData()
+        {
+           
+        }
+    }
+}
